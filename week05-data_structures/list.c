5d954f73a14659fcfabccf1913d2dc169d5be969
Marcos <marquinhos.dev@gmail.com>
2023-11-27 18:01:51 -0300

    finishing week 5

--- week05-data_structures/list.c
+++ week05-data_structures/list.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+typedef struct node
 {
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
+    int number;
+    struct node *next;
+}
+node;
+
+int main(int argc, char *argv[])
+{
+    node *list = NULL;
+
+    for (int i = 1; i < argc; i++)
     {
-        return 1;
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
     }
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = realloc(list, 4 * sizeof(int));
-    if (tmp == NULL)
+    node *ptr = list;
+    while (ptr != NULL)
     {
-        free(list);
-        return 1;
-    } 
-    
-    list = tmp;
-    list[3] = 4;
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
 
-    for (int i = 0; i < 3; i++)
+    ptr = list;
+    while(ptr != NULL)
     {
-        printf("%i\n", list[i]);
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
     }
 }
